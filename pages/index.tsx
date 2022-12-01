afa3625f0519a0e2b4ba5d0727b5cd0846fe036e
danghuutien <danghuutientk123@gmail.com>
2022-12-02 00:49:19 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,6 @@ import Header from '../components/Header'
 export default function Home() {
     return (
         <div className="bg-gray-300">
-            
-
             <Provider store={store}>
                 <div className='w-5/12 mx-auto'>
                     <Header />
