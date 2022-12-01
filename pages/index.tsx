8a7b50f32dca4678d959477ea57f55541a830055
danghuutien <danghuutientk123@gmail.com>
2022-12-01 10:21:38 +0700

    ssdsd

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,8 @@ export default function Home() {
         <div className="bg-gray-300">
             <Head>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"/>
-            <script src="https://cdn.tailwindcss.com"></script>
+            {/* 
+            <script src="https://cdn.tailwindcss.com"></script> */}
             </Head>
 
             <Provider store={store}>
